7d8d31071c58f89e8ccbdd99ea8047394841e849
rc2pc2 <petriccarcc@gmail.com>
2023-11-13 12:47:58 +0100

    livecoding addEventListener examples

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 // ? come aggiungere una classe ad un elemento esistente senza sovrascriverla
-const mainTitleElement = document.querySelector('h1.d-block');
-mainTitleElement.className = newElement.className + ' color-red';
+// const mainTitleElement = document.querySelector('h1.d-block');
+// mainTitleElement.className = newElement.className + ' color-red';
 
+const buttonElement = document.querySelector('button');
+console.log('Gino va al mare');
+
+buttonElement.addEventListener('click', function(){
+    console.log('inizio funzione di callback');
+    let somma = 5;
+
+    const outputElement = document.getElementById('output');
+    let outputElementValue = parseInt(outputElement.innerHTML);
+    somma = somma + outputElementValue;
+    outputElement.innerHTML = somma;
+
+    console.log('fine funzione di callback');
+});
+
+// console.log(somma); NOT DEFINED!!!!!
+
+console.log('Gina va in montagna');
+
+// Gino va al mare
+// Gina va in montagna
+// Quando clicchero' avro' i due console log rimanenti
+
+
+// Gino va al mare
+// Quando clicchero' avro' i due console log rimanenti
+// E SOLTANTO DOPO Gina va in montagna
