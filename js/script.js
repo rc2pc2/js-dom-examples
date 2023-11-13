a8e09387c11974ba5c5ed93b4999d6327340998a
rc2pc2 <petriccarcc@gmail.com>
2023-11-13 12:13:40 +0100

    refactoring title element name

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 // ? come aggiungere una classe ad un elemento esistente senza sovrascriverla
-const newElement = document.querySelector('h1.d-block');
-newElement.className = newElement.className + ' color-red';
+const mainTitleElement = document.querySelector('h1.d-block');
+mainTitleElement.className = newElement.className + ' color-red';
+
